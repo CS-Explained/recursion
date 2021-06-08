8f5c3c01d35309821eff6ec8748728e6b47bc246
CS-Explained <conceptcodepractice@gmail.com>
2021-06-08 13:12:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# recursion-cpp
+Introduction to recursion:
+1. Basic Concept
+1. Classical examples 
+    1. Factorial
+    1. Fibonacci series 
+Exercises:
+1. Adding up elements of an array
+1. Recursive linear search
+1. Binary search
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int fact(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    } 
+    return n * fact (n - 1);
+}
+int fib (int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+    return fib (n - 1) + fib (n - 2);
+}
+int main ()
+{
+    std::cout << fact(5) << std::endl;
+    std::cout << fib(5) << std::endl;
+    return 0;
+}
